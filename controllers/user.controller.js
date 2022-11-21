3e85925d949241d950170c222f6353ca00c0c1c8
arifvtti <arif.vtti@gmail.com>
2022-11-21 10:27:18 +0600

    create full server with get, post, delete, patch route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,94 @@ const { v4: uuidv4 } = require("uuid");
 const User = require("../models/user.model")
 
 
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully get users",
+            users
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Couldn't get users",
+            error: error.message
+        })
+    }
+}
+
+
+exports.getUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findOne({ id });
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully get user",
+            user
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Couldn't get user",
+            error: error.message
+        })
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findOne({ id });
+
+        user.name = req.body.name;
+        user.age = Number(req.body.age);
+
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully update user",
+            user
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Couldn't update user",
+            error: error.message
+        })
+    }
+}
+
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        await User.deleteOne({ id });
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully delete user"
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Couldn't delete user",
+            error: error.message
+        })
+    }
+}
+
 exports.createUser = async (req, res) => {
     try {
         const user = new User({
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 
 router.route("/")
+    .get(userController.getUsers)
     .post(userController.createUser)
+
+router.route("/:id")
+    .get(userController.getUser)
+    .delete(userController.deleteUser)
+    .patch(userController.updateUser)
 
 module.exports = router;
